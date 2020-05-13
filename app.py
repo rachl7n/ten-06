359ed02cc02000b6264b44eb71cd7953d0b33ac8
Rachael Claire Sullivan <rachaelclairesullivan@rachaels-mbp.home>
2020-05-13 10:50:46 -0400

    Adding basic templates

---
+++ app.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 21 14:57:17 2020
+
+@author: etill
+"""
+
+#import statements
+from flask import Flask, render_template
+
+#Flask app variable
+app = Flask(__name__)
+
+#static route
+@app.route("/")
+def test():
+    return render_template("index.html")
+
+@app.route("/1006")
+def ten_o_six(): 
+    return render_template("1006.html")
+
+@app.route("/personal")
+def personal(): 
+    return render_template("personal.html")
+
+
+#start the server
+if __name__ == "__main__":
+    app.run()
